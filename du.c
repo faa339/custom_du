a51705913d1364d182b5ed550776ec6feeaa5b50
Faris Alotaibi <faa339@nyu.edu>
2020-02-23 11:46:43 -0500

    TODO: change to int traverse, return filesize on each iteration

--- du.c
+++ du.c
@@ -7,33 +7,36 @@ the utility du. Usage is as follows:
 ./du [directoryname]
 
 */
-#define _XOPEN_SOURCE 500
+#define _DEFAULT_SOURCE
+#define MAX_SIZE 4096
+
 #include <stdlib.h>
 #include <stdio.h>
 #include <errno.h>
 #include <string.h>
 #include <dirent.h>
+#include <unistd.h>
 #include <sys/stat.h>
+#include <sys/types.h>
+
+
 
 void ErrorHandler(void);
 void DirTraverse(char* dirname);
 
-char pathname[4096];
 
 int main(int argc, char** argv)
 {
+	errno = 0;
 	if(argc>2)
 	{
 		fprintf(stderr, "Usage: %s [directory]\n", argv[0]);
 		exit(EXIT_FAILURE);
 	}
-	errno = 0;
-	char *directoryname = ".";
-	if(argc == 2)
-	{
-		directoryname = argv[1];
-	}
 	
+	char *directoryname = ".";
+	if(argc==2) directoryname = argv[1];
+
 	DirTraverse(directoryname);
 	exit(EXIT_SUCCESS);
 }
@@ -49,25 +52,28 @@ void DirTraverse(char* dirname)
 	DIR *directptr;
 	struct dirent *direntryptr;
 	struct stat statBuf;
-
-	if((directptr = opendir(dirname)) == NULL) ErrorHandler();
+	char temp[MAX_SIZE];
+	//Base case: we've tried opening something that isnt a directory
+	if((directptr = opendir(dirname)) == NULL) 
+		return;
 
 	while((direntryptr = readdir(directptr)) != NULL)
 	{
 		if(strcmp(direntryptr->d_name, ".") != 0 &&
 			strcmp(direntryptr->d_name, "..") != 0 )
 		{
-			lstat(direntryptr->d_name,&statBuf);
+			strcpy(temp, dirname);
+			strcat(temp, "/");
+			strcat(temp, direntryptr->d_name);
+			lstat(temp, &statBuf);
+			
 			if (S_ISDIR(statBuf.st_mode))
 			{
-				strcpy(pathname, dirname);
-				strcat(pathname,"/");
-				strcat(pathname, direntryptr->d_name);
-				printf("%ld\t%s\n",statBuf.st_size, pathname);
-				DirTraverse(pathname);
+				DirTraverse(temp);
+				printf("%ld\t%s\n",(statBuf.st_blocks), temp);
 			}
-		}	
-	}
 
+		}
+	}
 	closedir(directptr);
 }
